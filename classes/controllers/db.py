f7b5b53fd7ab562d68c54289891773a19bd2d4c7
doktor gauss <seppelicius@gmail.com>
2022-02-15 20:50:24 +0100

    add controllers

---
+++ classes/controllers/db.py
@@ -0,0 +1,69 @@
+
+'''
+    basic db class
+'''
+from distutils.dep_util import newer
+
+'''
+    base db controller for a given dictionary
+'''
+class DB:
+    '''
+        init db with an empty dictionary
+    '''
+    def __init__(self):
+        self._context = {}
+
+    '''
+        get a specific id element 
+        we assume each class provides an attribute named: 'entity_id'
+    '''
+    def get(self,id):
+        try:
+            # next gives the first occurance of this ................ condition
+            return self._context[id]
+        except:
+            print('error in db getter')
+
+
+    '''
+        update a specific id element
+    '''
+    def put(self, id, new_entity):
+        try:
+            if not id in self._context: raise ValueError('id not found')
+            self._context[id] = new_entity
+            return True
+        except:
+            print('error in db putter')
+            return False
+
+    '''
+        create a specific element
+    '''
+    def post(self, new_entity):
+        try:
+            #entity id is a must have, raise error if not given
+            if(new_entity.entity_id == None): raise ValueError('no entity id given')
+            if new_entity.entity_id in self._context: raise ValueError('id is taken')
+            self._context[new_entity.entity_id]= new_entity
+            return True
+        except:
+            print('error in db post')
+            return False
+
+    '''
+        get whole list
+    '''
+    def get_all(self):
+        try:   
+            return self._context.values()
+        except:
+            print('error in getAll')
+            return False
+
+    '''
+        get Dictionary
+    '''
+    def get_dict(self):
+        return self._context
---
+++ classes/controllers/review_controller.py
@@ -0,0 +1,29 @@
+from db import DB
+
+'''
+   Review Controller 
+'''
+class ReviewController(DB):
+    def __init__(self):
+        super(ReviewController, self).__init__()
+
+    def put(self, id, new_entity):
+        if(id == None): raise ValueError('id is required')
+        if(new_entity == None): raise ValueError('new entity is required')
+        if(new_entity.headline == None): raise ValueError('headline is required')
+        if(new_entity.text == None): raise ValueError('text is required')
+        if(new_entity.product_id == None): raise ValueError('product id is required')
+        return super(ReviewController, self).put(id, new_entity)
+    
+    def post(self, new_entity):
+        if(new_entity == None): raise ValueError('new entity is required')
+        if(new_entity.headline == None): raise ValueError('headline is required')
+        if(new_entity.text == None): raise ValueError('text is required')
+        if(new_entity.product_id == None): raise ValueError('product id is required')
+        return super(ReviewController, self).post(new_entity)
+
+    def get(self, id):
+        if id == None: raise ValueError('id is required')
+        return super(ReviewController, self).get(id)
+            
+    
